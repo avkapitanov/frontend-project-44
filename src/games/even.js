c1062416e0b40e2b29ff6e37987535549e9627bc
Artem Kapitanov <avkapitanov@yandex.ru>
2023-03-25 22:03:20 +0300

    refactor: add some constants

--- src/games/even.js
+++ src/games/even.js
@@ -1,11 +1,13 @@
 import { randomInteger, isEven } from '../utils.js';
-import { EVEN_MAX_NUMBER, EVEN_MIN_NUMBER } from '../const.js';
+import {
+  EVEN_MAX_NUMBER, EVEN_MIN_NUMBER, NEGATIVE_ANSWER_TEXT, POSITIVE_ANSWER_TEXT,
+} from '../const.js';
 
 export default () => ({
   questionText: 'Answer "yes" if the number is even, otherwise answer "no".',
   runRound: () => {
     const question = randomInteger(EVEN_MIN_NUMBER, EVEN_MAX_NUMBER);
-    const correctAnswer = isEven(question) ? 'yes' : 'no';
+    const correctAnswer = isEven(question) ? POSITIVE_ANSWER_TEXT : NEGATIVE_ANSWER_TEXT;
 
     return {
       question,
--- src/games/prime.js
+++ src/games/prime.js
@@ -1,11 +1,13 @@
 import { randomInteger, isPrime } from '../utils.js';
-import { PRIME_MAX_NUMBER, PRIME_MIN_NUMBER } from '../const.js';
+import {
+  NEGATIVE_ANSWER_TEXT, POSITIVE_ANSWER_TEXT, PRIME_MAX_NUMBER, PRIME_MIN_NUMBER,
+} from '../const.js';
 
 export default () => ({
   questionText: 'Answer "yes" if given number is prime. Otherwise answer "no".',
   runRound: () => {
     const question = randomInteger(PRIME_MIN_NUMBER, PRIME_MAX_NUMBER);
-    const correctAnswer = isPrime(question) ? 'yes' : 'no';
+    const correctAnswer = isPrime(question) ? POSITIVE_ANSWER_TEXT : NEGATIVE_ANSWER_TEXT;
 
     return {
       question,
